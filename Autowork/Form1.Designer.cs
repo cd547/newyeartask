3ba92c8b36fbd474ceab350cc973277232696070
cd547 <cd547@live.com>
2021-05-01 09:08:05 +0800

    first

---
+++ Autowork/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace Autowork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.msglog = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://sx.raofei.com.cn/stqrcode/auto/create";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "执行url";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "每日开奖生成";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "秒";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "秒";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "执行周期";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(310, 66);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 21);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            7200,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(168, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(75, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // msglog
+            // 
+            this.msglog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.msglog.BackColor = System.Drawing.Color.Purple;
+            this.msglog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.msglog.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.msglog.Location = new System.Drawing.Point(0, 118);
+            this.msglog.Name = "msglog";
+            this.msglog.ReadOnly = true;
+            this.msglog.Size = new System.Drawing.Size(863, 533);
+            this.msglog.TabIndex = 4;
+            this.msglog.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(439, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(412, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "任务2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "秒";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "秒";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(251, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "执行周期";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(310, 66);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(58, 21);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(168, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(75, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "开始";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(11, 12);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(319, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(10, 30);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 12);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "执行url";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(863, 651);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.msglog);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "自动化程序（async）";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox msglog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Autowork/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Autowork
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        ///访问网址
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns></returns>
+        public string HttpGet(string Url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                string retString = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+                return retString;
+            }
+            catch (WebException ex)//500内部错误
+            {
+                HttpWebResponse response = (HttpWebResponse)ex.Response;
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                string retString = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+                return retString;
+            }
+
+        }
+        class StateObject
+        {
+            public HttpWebRequest Request { get; set; }
+            public string Type{get;set;}
+        //可以添加任何你需要的东西
+        //……
+        }   
+    #region HttpWebRequest异步GET
+    public void AsyncGetWithWebRequest(string url,string type)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            StateObject state = new StateObject();
+            state.Request = request;
+            state.Type = type;
+            request.BeginGetResponse(new AsyncCallback(ReadCallback), state);
+        }
+        private  void ReadCallback(IAsyncResult asynchronousResult)
+        {
+            // var request = (HttpWebRequest)asynchronousResult.AsyncState;
+            StateObject state = (StateObject)asynchronousResult.AsyncState;
+            var request = state.Request;
+            try {
+                var response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+                using (var streamReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                {
+                    var resultString = streamReader.ReadToEnd();
+                    if (resultString != "0")
+                    { LogMessage("" + state.Type + "：" + resultString); }
+                      
+                }
+            }
+            catch (WebException ex)//500内部错误
+            {
+                HttpWebResponse response = (HttpWebResponse)ex.Response;
+                if (response != null)
+                    using (var streamReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                    {
+                        var resultString = streamReader.ReadToEnd();
+                        LogMessage(resultString);
+                    }
+                else {
+                    LogMessage("网络错误:"+ex.ToString());
+                }
+            }
+           
+        }
+        #endregion
+
+
+        #region 日志记录、支持其他线程访问 
+        public delegate void LogAppendDelegate(Color color, string text);
+        /// <summary> 
+        /// 追加显示文本 
+        /// </summary> 
+        /// <param name="color">文本颜色</param> 
+        /// <param name="text">显示文本</param> 
+        public void LogAppend(Color color, string text)
+        {
+            this.msglog.SelectionColor = color;
+            this.msglog.AppendText(text);
+            this.msglog.AppendText("\n");
+            //scroll滚到底部
+            this.msglog.SelectionStart = this.msglog.Text.Length;
+            this.msglog.SelectionLength = 0;
+            this.msglog.Focus();
+            //写入文件
+            try
+            {
+                if (System.IO.Directory.Exists("log"))
+                {
+                    string logFileName = @"log/LogName" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+                    using (TextWriter logFile = TextWriter.Synchronized(File.AppendText(logFileName)))
+                    {
+                        logFile.WriteLine(text);
+                        logFile.Flush();
+                        logFile.Close();
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory("log");//创建该文件
+                    string logFileName = @"log/LogName" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+                    using (TextWriter logFile = TextWriter.Synchronized(File.AppendText(logFileName)))
+                    {
+                        logFile.WriteLine(text);
+                        logFile.Flush();
+                        logFile.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Log Create Error:" + ex.Message.ToString());
+            }
+
+        }
+        /// <summary> 
+        /// 显示错误日志 
+        /// </summary> 
+        /// <param name="text"></param> 
+        public void LogError(string text)
+        {
+            LogAppendDelegate la = new LogAppendDelegate(LogAppend);
+            this.msglog.BeginInvoke(la, Color.Red, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + " Error:" + text);
+        }
+        /// <summary> 
+        /// 显示警告信息 
+        /// </summary> 
+        /// <param name="text"></param> 
+        public void LogWarning(string text)
+        {
+            LogAppendDelegate la = new LogAppendDelegate(LogAppend);
+            this.msglog.BeginInvoke(la, Color.Yellow, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + " Warning:" + text);
+        }
+        /// <summary> 
+        /// 显示信息 
+        /// </summary> 
+        /// <param name="text"></param> 
+        public void LogMessage(string text)
+        {
+            LogAppendDelegate la = new LogAppendDelegate(LogAppend);
+            this.msglog.BeginInvoke(la, Color.LightGray, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + " Message:" + text);
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LogMessage("自动化程序打开");
+            this.label2.Text = this.numericUpDown1.Value.ToString();
+            this.label9.Text = this.numericUpDown2.Value.ToString();
+           
+        }
+
+        //开始每日开奖生成
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.label2.Text = this.numericUpDown1.Value.ToString();
+            this.groupBox1.BackColor = Color.GreenYellow;
+            LogMessage("开始每日开奖生成");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(this.label2.Text) > 0)
+            {
+                this.label2.Text = (Convert.ToInt32(this.label2.Text) - 1).ToString();
+            }
+            else {
+                this.label2.Text = this.numericUpDown1.Value.ToString();
+                /////访问url
+                ///
+                AsyncGetWithWebRequest(this.textBox1.Text, "每日开奖生成：");
+                /*
+                string res=HttpGet(this.textBox1.Text);
+
+                if (res!="0")
+                {
+                    LogMessage("车位回收：" + res);
+                }
+               */
+
+            }
+        }
+        //停止每日开奖生成
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            this.groupBox1.BackColor = Color.White;
+            LogMessage("停止每日开奖生成");
+        }
+
+        //开始任务2
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+            this.label9.Text = this.numericUpDown2.Value.ToString();
+            this.groupBox2.BackColor = Color.GreenYellow;
+            LogMessage("开始任务2");
+        }
+
+        //停止任务2
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.timer2.Stop();
+            this.groupBox2.BackColor = Color.White;
+            LogMessage("停止任务2");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(this.label9.Text) > 0)
+            {
+                this.label9.Text = (Convert.ToInt32(this.label9.Text) - 1).ToString();
+            }
+            else
+            {
+                this.label9.Text = this.numericUpDown2.Value.ToString();
+                /////访问url
+                //string res = HttpGet(this.textBox2.Text);
+                AsyncGetWithWebRequest(this.textBox2.Text, "任务2：");
+                /*
+                if (res != "0")
+                {
+                    LogMessage("提前续费提醒：" + res);
+                }
+                */
+               
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LogMessage("自动化程序关闭");
+        
+  
+            //MessageBox.Show("");
+        }
+
+    }
+
+
+}
